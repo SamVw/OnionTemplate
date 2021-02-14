fc9909b613d9ae705ef65f7e3c518ce5e2e2331a
Sam Vanwelsenaere <sam.vanwelsenaere@hotmail.com>
2021-02-14 16:44:45 +0100

    Add project files.

---
+++ OnionTemplate.Application/Dtos/Template.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OnionTemplate.Application.Dtos
+{
+    public class Template
+    {
+    }
+}
---
+++ OnionTemplate/Controllers/TemplateController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace OnionTemplate.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TemplateController : ControllerBase
+    {
+        private readonly ILogger<TemplateController> _logger;
+
+        public TemplateController(ILogger<TemplateController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
